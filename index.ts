468569aad4dcbd2381fd1efb8262c78345554b2e
Jeremy Foster <jeremy.foster@live.com>
2018-06-03 05:27:55 -0700

    remove hub connection string property This is handled at the device level now, so each device gets a connection string

--- index.ts
+++ index.ts
@@ -8,11 +8,6 @@ module.exports = function (app: any) {
       type: 'object',
       required: ['IoTHubDeviceConnectionString'],
       properties: {
-        IoTHubDeviceConnectionString: {
-          type: 'string',
-          title: 'The Signal K Server device\'s device connection string',
-          description: 'You need to register your Signal K device in your Azure IoT Hub before it can communicate. Use the Azure Portal (http://portal.azure.com) or the Azure CLI to retrieve this device\'s connection string.'
-        },
         devices: {
           title: "Devices",
           type: "array",
