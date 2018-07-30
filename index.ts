ca660c959372743a786044a28b701177f6f6d720
Margarita Tagaban (InConsulting Inc.) <v-mataga@microsoft.com>
2018-07-30 09:35:00 -0700

    allowing user to input protocol of their choice.

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 let deviceAmqp = require('azure-iot-device-amqp');
+let deviceHttp = require('azure-iot-device-http');
+let deviceMqtt = require('azure-iot-device-mqtt');
 // let device = require('azure-iot-device');
 let Message = require('azure-iot-device').Message;
-
+let protocol;
 module.exports = function (app: any) {
 
   return {
@@ -31,6 +33,11 @@ module.exports = function (app: any) {
                 title: "Device Connection String",
                 require: true,
                 mask: true
+              },
+              deviceProtocol: {
+                type: "string",
+                title: "Device Protocol",
+                require: true
               }
             }
           }
@@ -39,19 +46,24 @@ module.exports = function (app: any) {
     },
     start: function (options: any) {
       console.log('Starting our plugin');
-
       let configuredDevices = [...options.devices];
-      app.signalk.on("delta", delta => delta.updates.forEach(update => {
 
-        //if the update is for a device that's been configured 
-        if (configuredDevices.some(d => d.deviceName == update.source.deviceName)) {
-          let configuredDevice = configuredDevices.find(d => d.deviceName == update.source.deviceName);
-          //if the client sdk doesn't already exist then create it
-          if (!(update.source.deviceName in configuredDevices)) {
-            configuredDevice.client = deviceAmqp.clientFromConnectionString(configuredDevices.find(d => d.deviceName == update.source.deviceName).deviceConnectionString);
-            configuredDevice.client.open()
+      app.signalk.on("delta", delta => delta.updates.forEach(update => {
+        p => {
+          protocol = p.deviceProtocol.toLowerCase();
+          if (protocol == 'amqp') protocol = deviceAmqp
+          if (protocol == 'mqtt') protocol = deviceMqtt
+          if (protocol == 'htpp') protocol = deviceHttp
+          //if the update is for a device that's been configured 
+          if (configuredDevices.some(d => d.deviceName == update.source.deviceName)) {
+            let configuredDevice = configuredDevices.find(d => d.deviceName == update.source.deviceName);
+            //if the client sdk doesn't already exist then create it
+            if (!(update.source.deviceName in configuredDevices)) {
+              configuredDevice.client = protocol.clientFromConnectionString(configuredDevices.find(d => d.deviceName == update.source.deviceName).deviceConnectionString);
+              configuredDevice.client.open()
+            }
+            configuredDevice.client.sendEvent(new Message(update))
           }
-          configuredDevice.client.sendEvent(new Message(update))
         }
       }))
     },
